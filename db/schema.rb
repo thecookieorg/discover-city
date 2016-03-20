facb5844ac13b9ca1e47eaafaeef0822d423df04
Marko Manojlovic <marko@thecookieapps.com>
2016-03-20 09:12:06 +0000

    Added extra user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320085740) do
+ActiveRecord::Schema.define(version: 20160320090949) do
 
   create_table "phones", force: :cascade do |t|
     t.string   "name"
@@ -63,6 +63,9 @@ ActiveRecord::Schema.define(version: 20160320085740) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
+    t.text     "bio"
+    t.integer  "age"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
